eecde6d95cdc3d11cfbdf7badf5e5e4ed6cbb344
Jonian Guveli <jonian@hardpixel.eu>
2018-07-28 09:53:00 +0300

    move acestream handler into separate class

---
+++ acestream.py
@@ -0,0 +1,169 @@
+# -*- coding: utf-8 -*-
+
+"""Acestream Engine: Communicate with the Acestream Engine HTTP API"""
+
+import os
+import json
+import time
+import signal
+import hashlib
+import threading
+import subprocess
+
+class Acestream(object):
+  """Acestream Engine"""
+
+  _observers = []
+
+  def __init__(self):
+    self._observers.append(self)
+    self._observed_events = []
+
+    self.live = False
+    self.poll = False
+    self.stdo = { 'stdout': subprocess.PIPE, 'stderr': subprocess.PIPE }
+
+  @property
+
+  def running(self):
+    """Check if acestream engine is running"""
+
+    status_url = self.get_url('webui/api/service', method='get_version', format='json')
+    req_output = self.request(status_url)
+    output_res = req_output.get('result', False)
+
+    return output_res is not False
+
+  def connect(self, event_name, callback_fn):
+    self._observed_events.append({ 'event_name': event_name, 'callback_fn': callback_fn })
+
+  def emmit(self, event_name, *callback_args):
+    for observer in self._observers:
+      for observable in observer._observed_events:
+        if observable['event_name'] == event_name:
+          observable['callback_fn'](*callback_args)
+
+  def request(self, url):
+    """Send engine API request"""
+
+    curl_proccess = subprocess.Popen(['curl', '-s', url], **self.stdo)
+    output, error = curl_proccess.communicate()
+
+    try:
+      return json.loads(output)
+    except ValueError:
+      return {}
+
+  def get_url(self, query_path, **params):
+    """Get engine API url"""
+
+    query_params = ['%s=%s' % (i, params[i]) for i in params.keys()]
+    query_params = '&'.join(query_params)
+
+    return 'http://127.0.0.1:6878/%s?%s' % (query_path, query_params)
+
+  def get_stream_url(self, url):
+    """Get engine API stream url"""
+
+    if url.startswith('http'):
+      stream_uid = hashlib.sha1(url.encode('utf-8')).hexdigest()
+      query_args = { 'url': url }
+    else:
+      stream_pid = url.split('://')[-1]
+      stream_uid = hashlib.sha1(stream_pid.encode('utf-8')).hexdigest()
+      query_args = { 'id': stream_pid }
+
+    return self.get_url('ace/getstream', format='json', sid=stream_uid, **query_args)
+
+  def update_stream_stats(self):
+    """Update stream statistics"""
+
+    if not self.poll:
+      return
+
+    req_output = self.request(self.stat_url)
+    output_res = req_output.get('response', False)
+    output_err = req_output.get('error', False)
+
+    if output_err:
+      return
+
+    for key in output_res.keys():
+      setattr(self, key, output_res[key])
+
+    if self.status == 'check':
+      return
+
+    if not self.live and self.status == 'dl':
+      self.live = True
+      time.sleep(2)
+
+    self.emmit('stats')
+
+  def poll_stream_stats(self):
+    """Update stream statistics"""
+
+    while self.poll:
+      time.sleep(1)
+      self.update_stream_stats()
+
+  def poll_stats(self):
+    """Run stream stats watcher in thread"""
+
+    thread = threading.Thread(target=self.poll_stream_stats)
+    thread.start()
+
+  def start_engine(self, args):
+    """Start acestream engine"""
+
+    if self.running:
+      return
+
+    try:
+      self.engine = subprocess.Popen(args, preexec_fn=os.setsid, **self.stdo)
+
+      while not self.running:
+        time.sleep(1)
+
+      self.emmit('message', 'running')
+    except OSError:
+      self.emmit('error', 'noengine', True)
+
+  def stop_engine(self):
+    """Stop acestream engine"""
+
+    if hasattr(self, 'engine'):
+      os.killpg(os.getpgid(self.engine.pid), signal.SIGTERM)
+
+  def open_stream(self, url, stats=False):
+    """Open acestream url"""
+
+    req_output = self.request(self.get_stream_url(url))
+    output_res = req_output.get('response', False)
+    output_err = req_output.get('error', False)
+
+    if output_err or not output_res:
+      self.emmit('error', 'unavailable', True)
+
+    for key in output_res.keys():
+      setattr(self, key, output_res[key])
+
+    self.emmit('message', 'waiting')
+
+    if stats:
+      self.poll = True
+      self.poll_stats()
+
+    while not self.live:
+      time.sleep(1)
+
+    self.emmit('message', 'started')
+
+  def close_stream(self):
+    """Close current stream"""
+
+    self.poll = False
+
+    if hasattr(self, 'command_url'):
+      stop_url = self.get_url(self.command_url, method='stop')
+      self.request(stop_url)
--- acestream_launcher.py
+++ acestream_launcher.py
@@ -5,14 +5,12 @@
 
 import os
 import sys
-import json
-import time
-import signal
 import shutil
-import hashlib
+import signal
 import argparse
-import threading
 import subprocess
+
+from acestream import Acestream
 
 class AcestreamLauncher(object):
   """Acestream Launcher"""
@@ -40,8 +38,6 @@ class AcestreamLauncher(object):
 
     self.atty = sys.stdin.isatty()
     self.args = parser.parse_args()
-    self.live = False
-    self.poll = True
     self.stdo = { 'stdout': subprocess.PIPE, 'stderr': subprocess.PIPE }
 
   @property
@@ -53,17 +49,6 @@ class AcestreamLauncher(object):
       self.libnotify = shutil.which('notify-send') is None
 
     return self.libnotify
-
-  @property
-
-  def running(self):
-    """Check if acestream engine is running"""
-
-    status_url = self.get_url('webui/api/service', method='get_version', format='json')
-    req_output = self.request(status_url)
-    output_res = req_output.get('result', False)
-
-    return output_res is not False
 
   def write(self, message):
     """Write message to stdout"""
@@ -98,143 +83,44 @@ class AcestreamLauncher(object):
     if terminate:
       self.quit()
 
-  def get_url(self, query_path, **params):
-    """Get engine API url"""
-
-    query_params = ['%s=%s' % (i, params[i]) for i in params.keys()]
-    query_params = '&'.join(query_params)
-
-    return 'http://127.0.0.1:6878/%s?%s' % (query_path, query_params)
-
-  def request(self, url):
-    """Send engine API request"""
-
-    curl_proccess = subprocess.Popen(['curl', '-s', url], **self.stdo)
-    output, error = curl_proccess.communicate()
-
-    try:
-      return json.loads(output)
-    except ValueError:
-      return {}
-
-  def get_stream_url(self):
-    """Get engine API stream url"""
-
-    if self.args.url.startswith('http'):
-      stream_uid = hashlib.sha1(self.args.url.encode('utf-8')).hexdigest()
-      query_args = { 'url': self.args.url }
-    else:
-      stream_pid = self.args.url.split('://')[-1]
-      stream_uid = hashlib.sha1(stream_pid.encode('utf-8')).hexdigest()
-      query_args = { 'id': stream_pid }
-
-    return self.get_url('ace/getstream', format='json', sid=stream_uid, **query_args)
-
-  def get_stream_stats(self):
-    """Get stream statistics"""
-
-    if not self.poll:
-      return
-
-    req_output = self.request(self.stat_url)
-    output_res = req_output.get('response', False)
-    output_err = req_output.get('error', False)
-
-    if output_err:
-      return
-
-    for key in output_res.keys():
-      setattr(self, key, output_res[key])
-
-    if self.status == 'check':
-      return
-
-    if not self.live and self.status == 'dl':
-      self.live = True
-      self.poll = self.atty
-
-      time.sleep(2)
-
-    if self.atty:
-      self.write_stream_stats()
-
-  def write_stream_stats(self):
+  def stats(self):
     """Print stream statistics"""
 
     labels = { 'dl': 'playing', 'prebuf': 'buffering' }
-    status = labels[self.status]
+    status = labels[self.engine.status]
 
     labels = 'down: %.1f kb/s up: %.1f kb/s peers: %d'
-    sstats = labels % (self.speed_down, self.speed_up, self.peers)
+    sstats = labels % (self.engine.speed_down, self.engine.speed_up, self.engine.peers)
 
-    if self.is_live:
+    if self.engine.is_live:
       label = 'LIVE - status: %s %s'
       stats = (status, sstats)
     else:
       label = 'VOD - status: %s total: %.2f%% %s'
-      stats = (status, self.total_progress, sstats)
+      stats = (status, self.engine.total_progress, sstats)
 
     self.write(label % stats)
-
-  def watch_stream_stats(self):
-    """Update stream statistics"""
-
-    while self.poll:
-      time.sleep(1)
-      self.get_stream_stats()
-
-  def watch_stream(self):
-    """Run stream watcher in thread"""
-
-    thread = threading.Thread(target=self.watch_stream_stats)
-    thread.start()
-
-  def start_engine(self):
-    """Start acestream engine"""
-
-    if self.running:
-      return
-
-    try:
-      engine_args = self.args.engine.split()
-      self.engine = subprocess.Popen(engine_args, preexec_fn=os.setsid, **self.stdo)
-
-      while not self.running:
-        time.sleep(1)
-
-      self.notify('running')
-    except OSError:
-      self.notify('noengine', True)
 
   def start_stream(self):
     """Strart streaming"""
 
-    req_output = self.request(self.get_stream_url())
-    output_res = req_output.get('response', False)
-    output_err = req_output.get('error', False)
+    self.engine = Acestream()
 
-    if output_err or not output_res:
-      self.notify('unavailable', True)
+    self.engine.connect('message', self.notify)
+    self.engine.connect('error', self.notify)
+    self.engine.connect('stats', self.stats)
 
-    for key in output_res.keys():
-      setattr(self, key, output_res[key])
-
-    self.notify('waiting')
-    self.watch_stream()
-
-    while not self.live:
-      time.sleep(1)
-
-    self.notify('started')
+    self.engine.start_engine(self.args.engine.split())
+    self.engine.open_stream(self.args.url, self.atty)
 
   def start_player(self):
     """Start media player"""
 
     player_args = self.args.player.split()
-    player_args.append(self.playback_url)
+    player_args.append(self.engine.playback_url)
 
     try:
-      self.player = subprocess.Popen(player_args, **self.stdo)
+      self.player = subprocess.Popen(player_args, preexec_fn=os.setsid, **self.stdo)
       self.player.communicate()
     except OSError:
       self.notify('noplayer', True)
@@ -242,31 +128,26 @@ class AcestreamLauncher(object):
   def run(self):
     """Start acestream and media player"""
 
-    try:
-      self.start_engine()
-      self.start_stream()
-      self.start_player()
-    except KeyboardInterrupt:
-      self.poll = False
-
+    self.start_stream()
+    self.start_player()
     self.quit()
 
-  def quit(self):
+  def quit(self, abort=False):
     """Stop acestream and media player"""
 
-    self.poll = False
+    self.engine.close_stream()
+    self.engine.stop_engine()
 
-    if hasattr(self, 'command_url'):
-      stop_url = self.get_url(self.command_url, method='stop')
-      self.request(stop_url)
-
-    if hasattr(self, 'engine'):
-      os.killpg(os.getpgid(self.engine.pid), signal.SIGTERM)
+    if abort and hasattr(self, 'player'):
+      os.killpg(os.getpgid(self.player.pid), signal.SIGTERM)
 
     print('\n\nExiting...')
     sys.exit()
 
 
 if __name__ == '__main__':
-  launcher = AcestreamLauncher()
-  launcher.run()
+  try:
+    launcher = AcestreamLauncher()
+    launcher.run()
+  except KeyboardInterrupt:
+    launcher.quit(True)
